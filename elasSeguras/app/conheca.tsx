87a086419c43cd761be61c7fac07508eedebae69
Ulysses Seabra <ulysses.seabra@ussolucoes.com.br>
2025-06-20 00:38:52 -0300

    Mudança para expo

---
+++ elasSeguras/app/conheca.tsx
@@ -0,0 +1,38 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from 'expo-image';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={require("@/assets/images/partial-react-logo.png")}
+        style={styles.reactLogo}
+      />
+      <Text>Elas Seguras</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
